03bccd265590ba62b4799403d31e9e8f46d1618a
Liu Yang <kisara@folate.li>
2025-12-29 09:11:11 +0800

    Move temperature dispersion analysis to notebook

---
+++ config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+STATION_PATH = Path("station.csv")
+SQL_PATH = Path("dump/china_data_insert.sql")
+FIGURE_DIR = Path("Figure")
+FIGURE_PNG_DIR = FIGURE_DIR / "png"
+FIGURE_SVG_DIR = FIGURE_DIR / "svg"
